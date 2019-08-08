7c3cc2359e78ee7058eea6d77fefe1cce079bb46
sinosoft <dongwenjie11@163.com>
2019-08-08 10:46:50 +0800

    去掉空格

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-//
+
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.example.*"})
 @MapperScan("com.example.mapper")
